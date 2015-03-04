9984df31b46111f23cf73d9a4d6a7580bb23ba59
Lesterpig <contact@lesterpig.com>
2015-03-04 13:20:08 +0100

    Test fix

--- test/pages.test.js
+++ test/pages.test.js
@@ -1,10 +1,12 @@
 require("blanket")();
 var app = require("../app.js");
 var request = require("supertest");
+var path = require("path");
 
 describe("Main Page", function() {
 
   before(function(done) {
+    __conf.dataPath = path.join(__dirname, "data");
     app.start(function() {
       done();
     });
