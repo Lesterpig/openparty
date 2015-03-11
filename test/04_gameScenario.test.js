1a1ac371eb5f62e0bc6dca88f640937de06061ea
Lesterpig <contact@lesterpig.com>
2015-03-11 21:20:54 +0100

    Add tests and fix bugs
    Close https://github.com/Lesterpig/openparty/issues/4

--- test/04_gameScenario.test.js
+++ test/04_gameScenario.test.js
@@ -1,8 +1,11 @@
 require("blanket")();
 var app = require("../app.js");
 var equals = require("assert").strictEqual;
+var assert = require("assert");
 var clientManager = require("./utils/clients.js");
 var path = require("path");
+
+var error = function() { throw new Error(); };
 
 describe("Game Scenario", function() {
 
@@ -28,27 +31,27 @@ describe("Game Scenario", function() {
   it("should init correctly", function(done) {
 
     var login = function() {
-        clients[0].on("loginResult", create);
-        for(var i = 0; i < 5; i++) {
-            clients[i].emit("login", {username: "p" + i, password: __conf.password});
-        }
+      clients[0].on("loginResult", create);
+      for(var i = 0; i < 5; i++) {
+          clients[i].emit("login", {username: "p" + i, password: __conf.password});
+      }
     };
 
     var create = function() {
-        clients[1].on("roomCreated", function(room) {
-            roomId = room.id;
-            join();
-        });
-        clients[0].emit("createRoom", {name: "Test Room", type: "raw"});
+      clients[1].on("roomCreated", function(room) {
+          roomId = room.id;
+          join();
+      });
+      clients[0].emit("createRoom", {name: "Test Room", type: "raw"});
     };
 
     var join = function() {
-        clients[3].on("roomJoined", function() {
-            done();
-        });
-        for(var i = 1; i < 4; i++) {
-            clients[i].emit("joinRoom", roomId);
-        }
+      clients[3].on("roomJoined", function() {
+          done();
+      });
+      for(var i = 1; i < 4; i++) {
+          clients[i].emit("joinRoom", roomId);
+      }
     };
 
     for(var i = 0; i < 6; i++) {
@@ -61,9 +64,351 @@ describe("Game Scenario", function() {
     }
   });
 
-  it("should start the game", function(done) {
-    clients[2].on("gameStarted", done);
+  it("should not start the game yet", function(done) {
+    clients[2].on("gameStarted", error);
+    clients[0].emit("startRoom");
+    setTimeout(done, 20);
+  });
+
+  it("should start the game, the first stage and send appropriate channel information", function(done) {
+    var room = require("../lib/rooms").rooms[0];
+    room.enableStart = true;
+    var i = 0;
+    var handleOk = function() {
+      if(++i >= 5) done();
+    }
+
+    clients[0].on("setAllowedChannels", function(data) {
+      if(!clients[0].firstAllowedChannels) {
+        return clients[0].firstAllowedChannels = true; // Note : the first event is always for general channel.
+      }
+      assert.deepEqual({
+        general: {r: true, w: true, n: "General"},
+        a: {r: true, w: true, n: "Channel A"}
+      }, data);
+      handleOk();
+    });
+    clients[2].on("setAllowedChannels", function(data) {
+      if(!clients[2].firstAllowedChannels) {
+        return clients[2].firstAllowedChannels = true;
+      }
+      assert.deepEqual({
+        general: {r: true, w: true, n: "General"},
+        b: {r: false, w: true, n: "Channel B"}
+      }, data);
+      handleOk();
+    });
+    clients[3].on("setAllowedChannels", function(data) {
+      if(!clients[3].firstAllowedChannels) {
+        return clients[3].firstAllowedChannels = true;
+      }
+      assert.deepEqual({}, data);
+      handleOk();
+    });
+    clients[2].on("setTimer", handleOk);
+    clients[2].on("gameStarted", handleOk);
     clients[0].emit("startRoom");
   });
 
+  it("should not start the game another time", function(done) {
+    clients[2].on("gameStarted", error);
+    clients[0].emit("startRoom");
+    setTimeout(done, 20);
+  });
+
+  describe("Chat Messages", function() {
+
+    it("should not emit chat messages in not-allowed channels", function(done) {
+
+      clients[0].on("messageSent", error);
+      clients[1].on("messageSent", error);
+      clients[3].on("messageSent", error);
+
+      clients[0].emit("sendMessage", {channel: "general"});
+      clients[0].emit("sendMessage", {channel: "general", message: ""});
+      clients[0].emit("sendMessage", {channel: "preChat", message: "hi"});
+      clients[1].emit("sendMessage", {channel: "b", message: "wrong message" });
+      clients[1].emit("sendMessage", {channel: "c", message: "wrong message" });
+      clients[3].emit("sendMessage", {channel: "general", message: "wrong message" });
+      setTimeout(done, 50);
+
+    });
+
+    it("should emit and receive chat messages in general", function(done) {
+
+      var i = 0;
+      var handleOk = function() {
+        if(++i >= 2) done();
+      }
+
+      clients[0].on("messageSent", handleOk);
+      clients[3].on("chatMessage", error);
+
+      clients[2].on("chatMessage", function(data) {
+        equals("p0", data.sender);
+        equals("Hello World!", data.message);
+        handleOk();
+      });
+
+      clients[0].emit("sendMessage", {channel: "general", message: "Hello World!" });
+
+    });
+
+    it("should emit and receive chat messages in roles channels", function(done) {
+      var i = 0;
+      var handleOk = function(data) {
+        equals("p0", data.sender);
+        equals("Hello Channel A!", data.message);
+        if(++i >= 2) done();
+      }
+
+      clients[0].on("chatMessage", handleOk);
+      clients[1].on("chatMessage", handleOk);
+
+      clients[2].on("chatMessage", error);
+      clients[3].on("chatMessage", error);
+
+      clients[0].emit("sendMessage", {channel: "a", message: "Hello Channel A!" });
+    });
+
+
+    it("should emit and receive chat messages in r/w only channels", function(done) {
+
+      var i = 0;
+      var handleOk = function(data) {
+        equals("p2", data.sender);
+        equals("Hello Channel B!", data.message);
+        if(++i >= 2) done();
+      }
+
+      clients[0].on("chatMessage", handleOk);
+      clients[1].on("chatMessage", handleOk);
+
+      clients[0].on("messageSent", error);
+      clients[2].on("chatMessage", error);
+      clients[3].on("chatMessage", error);
+
+      clients[0].emit("sendMessage", {channel: "b", message: "Wrong Message"});
+      clients[2].emit("sendMessage", {channel: "b", message: "Hello Channel B!"});
+
+    });
+
+    it("should remove role and its channel", function(done) {
+
+      clients[0].on("setAllowedChannels", error);
+      clients[1].on("setAllowedChannels", function(data) {
+        assert.deepEqual({
+          general: {r: true, w: true, n: "General"}
+        }, data);
+        clients[0].emit("sendMessage", {channel: "a", message: "you should not receive this message"});
+        setTimeout(done, 50);
+      });
+      clients[2].on("setAllowedChannels", function(data) {
+        assert.deepEqual({
+          general: {r: true, w: true, n: "General"},
+          b: {r: false, w: true, n: "Channel B"}
+        }, data);
+      });
+
+      clients[1].on("chatMessage", error);
+
+      clients[1].emit("sendMessage", {channel: "general", message: "remove_role_1"});
+      clients[2].emit("sendMessage", {channel: "general", message: "remove_role_1"}); // no effect
+
+    });
+
+    it("should be masked", function(done) {
+      var i = 0;
+      clients[0].on("chatMessage", function(data) {
+        equals("p0", data.sender);
+        equals("ok", data.message);
+        done();
+      });
+      clients[0].on("setAllowedChannels", function(channels) {
+        if(++i !== 2) return;
+        assert.deepEqual({
+          "general": {r: true, w: true, n: "General"},
+          "c": {r: true, w: true, n: "Channel C"}
+        }, channels);
+        clients[0].emit("sendMessage", {channel: "a", message: "ko"});
+        clients[0].emit("sendMessage", {channel: "b", message: "ko"});
+        clients[0].emit("sendMessage", {channel: "c", message: "ok"});
+      });
+      clients[0].emit("sendMessage", {channel: "general", message: "setup_channels"});
+    });
+
+    it("should be released", function(done) {
+
+      clients[0].on("chatMessage", function() {
+        done();
+      });
+
+      var player = require("../lib/rooms").rooms[0].players[0].player;
+      player.setChannel("a", null);
+      clients[0].emit("sendMessage", {channel: "a", message: "released"});
+    });
+  });
+
+  describe("Player information", function() {
+
+    it("should send some player information in channels", function(done) {
+      var room = require("../lib/rooms").rooms[0];
+      clients[0].on("playerInfo", function(data) {
+        equals("p0", data.username);
+        equals("- <strong>MASTER</strong> -", data.value);
+        done();
+      });
+      clients[2].on("playerInfo", error);
+      clients[3].on("playerInfo", error);
+      room.playerInfo("b", room.players[0], "- <strong>MASTER</strong> -");
+    });
+
+    it("should send some player information in general channel", function(done) {
+      var room = require("../lib/rooms").rooms[0];
+      clients[3].on("playerInfo", function() { done() });
+      room.playerInfo(room.players[1], "Slave");
+    });
+
+  });
+
+  describe("Actions", function() {
+
+    it("should not work if not available", function(done) {
+      clients[0].on("chatMessage", error);
+      clients[0].emit("executeAction");
+      clients[0].emit("executeAction", {});
+      clients[1].emit("executeAction", {action: "button"});
+      clients[4].emit("executeAction", {action: "button"});
+      setTimeout(done, 20);
+    });
+
+    describe("Button", function() {
+
+      it("should execute correctly", function(done) {
+        clients[0].on("chatMessage", function() { done() });
+        clients[0].emit("executeAction", {action: "button"});
+      });
+
+      it("should work correctly with individual actions", function(done) {
+        clients[2].on("chatMessage", function() {
+          done();
+        });
+        clients[2].emit("sendMessage", { message: "setup_action", channel: "general" });
+        setTimeout(function() {
+          clients[2].emit("executeAction", {action: "test"});
+        }, 50);
+      });
+
+      it("should be masked", function(done) {
+        clients[0].on("chatMessage", error);
+
+        var player = require("../lib/rooms").rooms[0].players[0].player;
+        player.setAction("button", {}); //here, we disabled the action
+        clients[0].emit("executeAction", {action: "button"});
+
+        setTimeout(done, 20);
+      });
+
+      it("should be released", function(done) {
+        clients[0].on("chatMessage", function() { done() });
+
+        var player = require("../lib/rooms").rooms[0].players[0].player;
+        player.setAction("button", null); //here, we released the action mask
+        clients[0].emit("executeAction", {action: "button"});
+      });
+
+    });
+
+    describe("Select", function() {
+      it("should not accept invalid entries/players", function(done) {
+        clients[0].on("chatMessage", error);
+        clients[0].emit("executeAction", {action: "chooseFn"});
+        clients[0].emit("executeAction", {action: "chooseFn", value: "1"});
+        clients[0].emit("executeAction", {action: "choose"});
+        clients[0].emit("executeAction", {action: "choose", value: "Three"});
+        clients[0].emit("executeAction", {action: "choose", value: "p0"});
+        clients[0].emit("executeAction", {action: "player"});
+        clients[0].emit("executeAction", {action: "player", value: "invalid"});
+        setTimeout(done, 20);
+      });
+
+      it("should accept valid entries", function(done) {
+        clients[0].on("chatMessage", function(data) {
+          equals("One", data.message);
+          done();
+        });
+        clients[0].emit("executeAction", {action: "choose", value: "One"});
+      });
+
+      it("should accept valid entries with types", function(done) {
+        clients[0].on("chatMessage", function(data) {
+          equals(1, data.message);
+          done();
+        });
+        clients[0].emit("executeAction", {action: "chooseFn", value: 1});
+      });
+
+      it("should accept valid players", function(done) {
+        clients[0].on("chatMessage", function(data) {
+          equals("p1", data.message);
+          done();
+        });
+        clients[0].emit("executeAction", {action: "player", value: "p1"});
+      });
+    });
+
+  });
+
+  describe("Stages", function() {
+
+    it("should start a new infinite stage", function(done) {
+      var room = require("../lib/rooms").rooms[0];
+      clients[0].on("clearTimer", done);
+      room.nextStage("infinite");
+    });
+
+    it("should change the stage duration", function(done) {
+      var room = require("../lib/rooms").rooms[0];
+      clients[0].on("setTimer", function(duration) {
+        equals(1, duration);
+        done();
+      });
+      room.setStageDuration(1);
+    });
+
+    it("should start a short stage and end it to the previous stage", function(done) {
+      var room = require("../lib/rooms").rooms[0];
+      clients[0].on("clearTimer", done);
+      room.nextStage("short");
+    });
+
+  });
+
+  describe("Disconnection", function() {
+
+    it("should handle ingame disconnections", function(done) {
+      var room   = require("../lib/rooms").rooms[0];
+      var player = room.resolveUsername("p3").player;
+      clients[3].disconnect();
+      setTimeout(function() {
+        player.emit("this message will not crash");
+        assert(null === room.resolveUsername("p3"));
+        done();
+      }, 100);
+    });
+
+    it("should not leave if not in room", function() {
+      /**
+       * This case should never happen.
+       * It's a kind of "last protection" before crash, that's why we need a test
+       */
+      require("../lib/rooms").leaveRoom({
+        invalid: "socket",
+        currentRoom: {
+          players: ["a"]
+        }
+      });
+    });
+
+  });
 });
--- test/data/raw/definition.js
+++ test/data/raw/definition.js
@@ -1,26 +1,153 @@
 module.exports = function() {
 
-    return {
-        name: "Test",
-        description: "A Test Gamepley",
-
-        minPlayers: 4,
-        maxPlayers: 6,
-
-        parameters: [
-            {
-                name: "Number of things",
-                type: Number,
-                value: 1,
-                help: "-"
-            }
-        ],
-
-        init: function() {},
-
-        start: function(room, callback) {
-            callback(null);
+  var role1 = {
+    channels: {
+      a: {r: true, w: true, n: "Channel A"},
+      b: {r: true, w: false,n: "Channel B"}
+    },
+    actions: {
+      button: {
+        isAvailable: function(player) {
+          return player.socket.username === "p0";
+        },
+        type: "button",
+        options: {
+          submit: "Choose",
+        },
+        execute: function(player) {
+          player.room.message("executed_ok");
         }
+      },
+      player: {
+        isAvailable: function(player) {
+          return true;
+        },
+        type: "select",
+        options: {
+          submit: "Choose",
+        },
+        execute: function(player, choice) {
+          player.room.message(choice.socket.username);
+        }
+      },
+      choose: {
+        isAvailable: function(player) {
+          return true;
+        },
+        type: "select",
+        options: {
+          choices: ["One", "Two"],
+          submit: "Choose",
+        },
+        execute: function(player, choice) {
+          player.room.message("general", choice);
+        }
+      },
+      chooseFn: {
+        isAvailable: function(player) {
+          return player.room.currentStage === "stageA";
+        },
+        type: "select",
+        options: {
+          choices: function() { return [1,2,3]; },
+          submit: "Choose",
+        },
+        execute: function(player, choice) {
+          player.room.message("general", choice);
+        }
+      }
     }
+  };
+
+  var role2 = {
+    channels: {
+      b: {r: false, w: true, n: "Channel B"}
+    },
+    actions: {}
+  }
+
+  return {
+    name: "Test",
+    description: "A Test Gamepley",
+
+    minPlayers: 4,
+    maxPlayers: 6,
+    firstStage: "stageA",
+
+    parameters: [
+      {
+        name: "Number of things",
+        type: Number,
+        value: 1,
+        help: "-"
+      }
+    ],
+
+    stages: {
+      "stageA": {
+        start: function(room, cb) {
+          cb(null, 5);
+        },
+        end: function(room) {}
+      },
+      "infinite": {
+        start: function(room, cb) {
+          cb(null, -1);
+        },
+        end: function(room) {}
+      },
+      "short": {
+        start: function(room, cb) {
+          cb(null, 0.2);
+        },
+        end: function(room) {
+          room.nextStage("infinite");
+        }
+      }
+    },
+
+    processMessage: function(channel, message, player) {
+      if(message === "remove_role_1") {
+        player.setRole("role1", null);
+        return false;
+      } else if(message === "setup_action") {
+        player.setAction("test", {
+          isAvailable: function(player) {
+            return true;
+          },
+          type: "button",
+          options: {
+            submit: "yolo",
+          },
+          execute: function(player) {
+            player.room.message("executed_ok");
+          }
+        });
+      } else if(message === "setup_channels") {
+        player.setChannel("a", {r: false, w: false});
+        player.setChannel("c", {r: true, w: true, n: "Channel C"});
+      } else {
+        return message;
+      }
+    },
+
+    init: function() {},
+
+    onDisconnect: function(room, player) {
+      if(player.room !== room)
+        throw new Error("Invalid callback");
+    },
+
+    start: function(room, callback) {
+
+      if(!room.enableStart) return callback("not_enabled");
+
+      room.players[0].player.setRole("role1", role1);
+      room.players[1].player.setRole("role1", role1);
+      room.players[2].player.setRole("role2", role2);
+      room.players[3].player.setChannel("general", {r: false, w: false}); // Disable all channels
+      callback(null);
+    }
+  }
 
 };
